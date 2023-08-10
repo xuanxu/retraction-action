54cdf096e4758b24029fb406b1ad49e2af125ff4
Juanjo Bazán <jjbazan@gmail.com>
2023-08-10 16:13:15 +0200

    Simplify using new TheOJ::RetractionNotice object

--- prepare_retraction_metadata.rb
+++ prepare_retraction_metadata.rb
@@ -12,30 +12,9 @@ begin
 
   journal = Theoj::Journal.new(journal_data)
   doi = journal.paper_doi_for_id(journal.paper_id_from_issue(issue_id))
-  paper = Theoj::PublishedPaper.new(doi)
-  paper_metadata = paper.metadata
-
-  retraction_metadata = {
-    title: "Retraction notice for: " + paper_metadata[:title],
-    tags: paper_metadata[:tags],
-    authors: [{ given_name: journal_alias.upcase,
-                last_name: "Editorial Board",
-                affiliation: journal.name}],
-    doi: paper_metadata[:doi] + "R",
-    software_repository_url: paper_metadata[:software_repository],
-    reviewers: [],
-    volume: journal.current_volume,
-    issue: journal.current_issue,
-    year: journal.current_year,
-    page: paper_metadata[:page] + "R",
-    journal_alias: journal_alias,
-    software_review_url: paper_metadata[:paper_review],
-    archive_doi: paper_metadata[:software_archive].to_s.gsub("https://doi.org/", ""),
-    editor: { github_user: "openjournals", name: "Editorial Board", url: journal.data[:url] },
-    submitted_at: Time.now.strftime("%Y-%m-%d"),
-    published_at: Time.now.strftime("%Y-%m-%d")
-  }
-  retraction_metadata[:citation_string] = "#{journal_alias.upcase} Editorial Board, (#{retraction_metadata[:year]}). #{retraction_metadata[:title]}. #{journal.name}, #{retraction_metadata[:volume]}(#{retraction_metadata[:issue]}), #{issue_id + "R"}, https://doi.org/#{retraction_metadata[:doi]}"
+  retraction_notice = Theoj::RetractionNotice.new(doi, journal_alias)
+  retraction_metadata = retraction_notice.metadata
+  retraction_metadata[:citation_string] = retraction_notice.citation_string
 
 rescue Theoj::Error => e
   raise " ❌ Error: #{e.message}"
